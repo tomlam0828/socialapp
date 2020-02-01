f4fb9a0f5c9e9d4b8eb6f86c89aa1f5a1b5797fc
Tom Lam <tomlam0828@gmail.com>
2020-02-01 00:15:47 -0800

    push

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 // Load modules
 const express = require('express');
 const app = express();
-const port = process.env.PORT ||  3000;
+const port = process.env.PORT || 3000;
 const exphbs = require('express-handlebars');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const User = require('./models/user');
 
 // connect to MongoURI exported from external file
 const keys = require('./config/keys');
+require('./passport/google-passport');
 const mongoose = require('mongoose');
+const passport = require('passport');
+// express config
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to database
 mongoose.connect(keys.MongoURI, {
@@ -15,6 +32,11 @@ mongoose.connect(keys.MongoURI, {
     console.log('Connected to database.....');
 }).catch((err) => {
     console.log(err);
+})
+
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 })
 
 // setup template engine
@@ -27,12 +49,43 @@ app.set('view engine', 'handlebars');
 // setup static file to serve css
 app.use(express.static('public'));
 
+// handle routes
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
+})
+
+// google auth route
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope:
+            ['profile', 'email']
+    }
+    ));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        res.redirect('/profile');
+    });
+
+app.get('/profile', (req, res) => {
+    User.findById({ _id: req.user._id }).then((user) => {
+        res.render('profile', {
+            user: user
+        });
+    })
+});
+
+//user logout
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 })
 
 app.listen(port, () => {
