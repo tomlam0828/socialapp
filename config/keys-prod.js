e3187e272839d353e49a79a3a2f19ff77d076d71
Tom Lam <tomlam0828@gmail.com>
2020-02-01 00:32:33 -0800

    push

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://socialdatabase:Yclam19950828@online-xfxe8.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '313669717606-kv3l552f00logg1h8blr94c37kq5b5he.apps.googleusercontent.com',
-    GoogleClientSecret: 'aUMdM-MDEbf4WMU903Bl6Kk7',
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
