7f79c064101e1abffd90db11b207c66597fe828d
Tom Lam <tomlam0828@gmail.com>
2020-02-01 00:26:02 -0800

    push

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://socialdatabase:Yclam19950828@online-xfxe8.mongodb.net/test?retryWrites=true&w=majority',
+    GoogleClientID: '313669717606-kv3l552f00logg1h8blr94c37kq5b5he.apps.googleusercontent.com',
+    GoogleClientSecret: 'aUMdM-MDEbf4WMU903Bl6Kk7',
+}
