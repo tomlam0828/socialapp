da87c5d09999d7dbe22d22fe52897ed543c9e8b5
Tom Lam <tomlam0828@gmail.com>
2020-02-01 18:03:20 -0800

    push

--- models/post.js
+++ models/post.js
@@ -16,7 +16,7 @@ const postSchema = new Schema({
         type: String,
         default: 'public'
     },
-    allowComment: {
+    allowComments: {
         type: Boolean,
         default: true
     },
